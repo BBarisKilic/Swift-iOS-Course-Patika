3ae5f770137d20fc3fcb594562be7166ea6eceb9
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-08-05 15:22:21 +0300

    first commit

---
+++ 01-VarAndLet.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello,"
+var isim = "Baris"
+isim = "Oznur"
+print("\(greeting) \(isim)")
+
+let pi: Float = 3.14
+print(pi)
+
+var bosDegisken: String
+let bosSabit: Int
+
